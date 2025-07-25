2286b3867e54af16c259881169fb493aaa9438ef
buenno <15213524+Buenno@users.noreply.github.com>
2025-07-25 09:47:12 +0100

    feat: added plaster scaffolding templates

---
+++ Plaster Templates/FunctionTemplate/root/function.ps1
@@ -0,0 +1,40 @@
+# function template
+<%
+"Function $PLASTER_PARAM_FunctionName {"
+%>
+<%
+@"
+  <#
+    .SYNOPSIS
+      Short description
+    .DESCRIPTION
+      Long description
+    .PARAMETER XXX
+      Describe the parameter
+    .EXAMPLE
+      Example of how to use this cmdlet
+    .NOTES
+      Insert any notes
+    .LINK
+      Insert links
+  #>
+
+    [CmdletBinding()]
+    Param(
+      [Parameter(
+            Mandatory = $false,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            Position = 0
+            )]
+      [string] $ParameterName
+    )
+
+  BEGIN {}
+
+  PROCESS {}
+      
+  END {}
+}
+"@
+%>
